dca120fe17da5cef21c998deeadd7a53e7da9abd
Linyi Li <linyi.li@emory.edu>
2021-03-15 21:27:17 +0800

    Update DataCleaning.py and FilteredData

--- DataCleaning.py
+++ DataCleaning.py
@@ -14,6 +14,8 @@ out_dir = dir+'/FilteredData/'
 list_of_dct = []
 unavailable_urls = []
 nlp = spacy.load('en_core_web_sm')
+dataset = pd.read_excel('Master Modeler Competition 2021.xlsx', usecols='D', names = ['url']) 
+all_urls = dataset['url']
 
 '''
 Meaning of each field in dict:
@@ -33,6 +35,7 @@ Meaning of each field in dict:
     attachments: list of dict, list of dictionary that represents the info of each media (normally there is only one media, but in case there are multiple, I will use a list)
         media_type: str, type of the media
         media_url: str, url of the media (photo/sharing url depending on the media_type) attached to the post
+            list of url if media_type is album
         media_description: str, title of media if media_type is not photo, else None
         thumbnail_url: str, url of the thumbnail if media_type is not photo, else None
     --- Note: 
@@ -46,10 +49,11 @@ for file in raw_files:
         new_dct = {'url': None, 'created_time': None, 'message': None, 'cleaned_message': None, 'emojis': None, 'mentions': None, 'names': None, 
                    'message_tags': None, 'attachments': None}
         filename = os.path.basename(file)
+        actual_url = all_urls[int(filename[:-5])]
         # print(filename)
         try:
             dct = json.load(f)
-            new_dct['url'] = dct['permalink_url']
+            new_dct['url'] = actual_url
             new_dct['created_time'] = dct['created_time']
             if 'message' in dct and dct['message'] is not None:
                 new_dct['message'] = dct['message']
@@ -64,11 +68,11 @@ for file in raw_files:
                 pr.remove_special_characters()
                 new_dct['names'] = pr.retrieve_names()
                 pr.fully_preprocess()
-                new_dct['cleaned_message'] = pr.text.split()
+                new_dct['cleaned_message'] = pr.text.split() if len(pr.text)>0 else None
                 # print('cleaned text')
             if 'message_tags' in dct:
                 message_tags = dct['message_tags']
-                new_dct['message_tags'] = [i['name'].replace('#','') for i in message_tags]
+                new_dct['message_tags'] = [i['name'].replace('#','') for i in message_tags] if len(message_tags)>0 else None
             if 'attachments' in dct:
                 attachments = dct['attachments']['data']
                 new_dct['attachments'] = [{'media_type': None, 'media_url': None, 'media_description': None, 'thumbnail_url': None} for _ in range(len(attachments))]
@@ -77,17 +81,22 @@ for file in raw_files:
                     if 'url' in attachment:
                         new_dct['attachments'][i]['media_url'] = attachment['url']
                     image = None
-                    if 'media' in attachment and 'image' in attachment['media']['image']:
+                    if 'media' in attachment and 'image' in attachment['media']:
                         image = attachment['media']['image']
                     if image and attachment['type'] == 'photo':
                         new_dct['attachments'][i]['media_url'] = image['src']
+                    elif attachment['type'] == 'album':
+                        subattachments = attachment['subattachments']['data']
+                        new_dct['attachments'][i]['media_url'] = [s['media']['image']['src'] for s in subattachments]
+                        # print(new_dct['attachments'][i]['media_url'])
                     else:
                         if 'url' in attachment:
                             new_dct['attachments'][i]['media_url'] = attachment['url']
-                        if image and 'src' in image:
+                        if image and 'src' in image.keys():
                             new_dct['attachments'][i]['thumbnail_url'] = image['src']
                         if 'title' in attachment:
                             new_dct['attachments'][i]['media_description'] = attachment['title']
+                        # print(new_dct['attachments'][i]['thumbnail_url'])
             out_file = open(out_dir+filename, 'w')
             json.dump(new_dct, out_file)
             list_of_dct.append(new_dct)
